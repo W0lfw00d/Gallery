61ef482a1d8c4dd82dacf3e6a6dd46335471b89e
Wolfwood <mdenheyer@gmail.com>
2014-03-25 12:18:15 +0100

    Removed "barryvdh/laravel-migration-generator" package because of version/dependency conflicts, set debug to false

--- app/config/app.php
+++ app/config/app.php
@@ -13,7 +13,7 @@ return array(
 	|
 	*/
 
-	'debug' => true,
+	'debug' => false,
 
 	/*
 	|--------------------------------------------------------------------------
@@ -106,8 +106,8 @@ return array(
         'Illuminate\Validation\ValidationServiceProvider',
         'Illuminate\View\ViewServiceProvider',
         'Illuminate\Workbench\WorkbenchServiceProvider',
-		'JasonNZ\LaravelGrunt\LaravelGruntServiceProvider',
-		'Barryvdh\Debugbar\ServiceProvider',
+    		'JasonNZ\LaravelGrunt\LaravelGruntServiceProvider',
+    		'Barryvdh\Debugbar\ServiceProvider',
 	),
 
 	/*
@@ -173,7 +173,7 @@ return array(
         'URL'             => 'Illuminate\Support\Facades\URL',
         'Validator'       => 'Illuminate\Support\Facades\Validator',
         'View'            => 'Illuminate\Support\Facades\View',
-		'Grunt'			  => 'JasonNZ\LaravelGrunt\LaravelGruntServiceProvider',
+		    'Grunt'			  => 'JasonNZ\LaravelGrunt\LaravelGruntServiceProvider',
 	),
 
 );
